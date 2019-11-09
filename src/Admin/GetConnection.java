e929e47210364c5328b5924fd2266e7ed5d1aa43
ifzal <ifzal@ifzalkola>
2019-11-09 16:33:19 +0530

    new commit

---
+++ src/Admin/GetConnection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author ifzal
+ */
+public class GetConnection {
+    public ResultSet exQu(String query) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+          Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinemadb","root","Bilal_1053");
+          Statement st = con.createStatement();
+          ResultSet rs = st.executeQuery(query);
+        
+        return rs;
+    }
+}
